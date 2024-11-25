e2ab5a284ad1f28dd3c9b37445b80d544a185fd5
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-26 00:46:13 +0300

    Added rabbitmq message sending to api

--- src/services/api/service.py
+++ src/services/api/service.py
@@ -1,17 +1,35 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+import pika
+from fastapi import FastAPI, HTTPException, status
 
 from config import settings
 from services.api.schemas import ProcessMessageRequest
 
+QUEUE_NAME = "api2filter"
+
+connection: pika.BlockingConnection | None = None
+channel: pika.adapters.blocking_connection.BlockingChannel | None = None
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):  # noqa
+    global connection, channel
+
     # before startup
-    print(settings.RABBITMQ_HOST)
+    print("Starting RabbitMQ connection...")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
     yield
+
+    print("Stopping RabbitMQ connection...")
     # before shutdown
+    if channel:
+        channel.close()
+    if connection:
+        connection.close()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -19,4 +37,13 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/process-message")
 def process_message(body: ProcessMessageRequest):
-    return None
+    try:
+        channel.basic_publish(
+            exchange="",
+            routing_key=QUEUE_NAME,
+            body=body.model_dump_json(),
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Failed to publish message to RabbitMQ: {str(e)}")
