a388d3405295aa3d7aa82b2c585e775bd6f613f5
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-26 02:09:36 +0300

    Add email stuff to config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-from pydantic import FilePath
+from pydantic import FilePath, EmailStr, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,6 +9,12 @@ class Settings(BaseSettings):
     QUEUE1: str = "api2filter"
     QUEUE2: str = "filter2screaming"
     QUEUE3: str = "screaming2publish"
+
+    SMTP_SERVER: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASSWORD: SecretStr
+    EMAIL_RECEIVERS: list[EmailStr] = []
 
     model_config = SettingsConfigDict(env_file=".env")
 
