bac4108a5d0bedfeea183c08e40a4e4d00f09038
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-26 01:15:46 +0300

    Added filter service

---
+++ src/services/filter/service.py
@@ -0,0 +1,55 @@
+import json
+
+import pika
+
+from config import settings
+from services.schemas import Message
+
+INPUT_QUEUE_NAME = "api2filter"
+OUTPUT_QUEUE_NAME = "filter2screaming"
+BLACKLIST_WORDS: set[str] = set()
+
+
+def callback(ch, method, properties, body):
+    try:
+        message = Message(**json.loads(body))
+
+        message_text_set = set(message.message_text.lower().split())
+        if not message_text_set.intersection(BLACKLIST_WORDS):
+            ch.basic_publish(
+                exchange="",
+                routing_key=OUTPUT_QUEUE_NAME,
+                body=message.model_dump_json(),
+                properties=pika.BasicProperties(delivery_mode=2)
+            )
+    except Exception as e:
+        print(f"Error while processing message {body=}: {str(e)}")
+    finally:
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def parse_blacklist_words():
+    global BLACKLIST_WORDS
+
+    with open(settings.WORDS_BLACKLIST_PATH, mode="r") as fp:
+        BLACKLIST_WORDS = set(map(lambda x: x.strip().lower(), fp.readlines()))
+
+
+def main():
+    parse_blacklist_words()
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=INPUT_QUEUE_NAME, durable=True)
+    channel.queue_declare(queue=OUTPUT_QUEUE_NAME, durable=True)
+
+    channel.basic_consume(queue=INPUT_QUEUE_NAME, on_message_callback=callback)
+
+    print("Filter service has started")
+
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    main()
