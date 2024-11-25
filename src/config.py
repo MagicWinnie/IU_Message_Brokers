2d495307f53f2275e573b98ff7cd7f19e79b0249
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-26 00:51:41 +0300

    Added words blacklist file

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
+from pydantic import FilePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     RABBITMQ_HOST: str = "localhost"
+    WORDS_BLACKLIST_PATH: FilePath
 
     model_config = SettingsConfigDict(env_file=".env")
 
