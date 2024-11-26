4b175766a32d6158b58323fabddd670217643e1f
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-11-26 03:27:26 +0300

    Testing more stuff

--- src/services/publish/service.py
+++ src/services/publish/service.py
@@ -21,7 +21,7 @@ def callback(ch, method, properties, body):
         msg["To"] = ", ".join(settings.EMAIL_RECEIVERS)
 
         with smtplib.SMTP(settings.SMTP_SERVER, settings.SMTP_PORT) as server:
-            server.ehlo(settings.SMTP_EMAIL)
+            server.ehlo()
             server.starttls()
             server.login(settings.SMTP_EMAIL, settings.SMTP_PASSWORD)
             server.sendmail(settings.SMTP_EMAIL, settings.EMAIL_RECEIVERS, msg.as_string())
